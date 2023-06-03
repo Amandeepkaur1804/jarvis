19fa7d9edff27dde3626a230ab047efee405bfa5
Amandeep Kaur <107187322+Amandeepkaur1804@users.noreply.github.com>
2023-06-03 16:49:26 +0530

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -50,14 +50,6 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
-
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -83,12 +75,6 @@ if __name__ == "__main__":
             webbrowser.open("stackoverflow.com")   
 
 
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
@@ -97,15 +83,4 @@ if __name__ == "__main__":
             codePath = "C:\\Users\\Surinder\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'email to harry' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
-
-           
+       
